efa49175a372f501ff6d734502c38c2183f54507
Dg3k4 <mikle.krasnov.2005@gmail.com>
2026-08-24 23:35:49 +0500

    feat(auth): add check PIN session during refresh

---
+++ client/src/components/dashboard/DashboardOverview.jsx
@@ -0,0 +1,14 @@
+import {} from 'react';
+import "./dashboard.scss"
+
+const DashboardOverview = () => {
+    return (
+        <div className="dashboard">
+            <div className="dashboard__container">
+
+            </div>
+        </div>
+    );
+};
+
+export default DashboardOverview;
